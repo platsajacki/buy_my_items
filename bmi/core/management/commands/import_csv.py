5f3859b3151637c04483d07270c8cabeea5b9bb8
Вячеслав Менюхов <menyukhov@bk.ru>
2024-03-03 20:32:37 +0300

    Update import_csv.

--- bmi/core/management/commands/import_csv.py
+++ bmi/core/management/commands/import_csv.py
@@ -4,7 +4,7 @@ from typing import Any
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from items.models import Tax
+from items.models import Item, Tax
 from orders.models import Discount
 
 DATA_DIR: str = f'{settings.BASE_DIR}/static/data'
@@ -16,6 +16,7 @@ class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> None:
         self.import_coupons()
         self.import_product_tax_codes()
+        self.import_items()
 
     def import_data_from_csv(
         self, file_path: str, model_class: type[Discount] | type[Tax], field_names: list[str]
@@ -25,14 +26,16 @@ class Command(BaseCommand):
             for row in reader:
                 data: dict[str, str] = dict(zip(field_names, row))
                 instance = model_class.objects.filter(**data)
+                if model_class == Item:
+                    data['tax'] = Tax.objects.get(id=data['tax'])
                 if not instance.exists():
                     instance.create(**data)
                     self.stdout.write(
-                        self.style.SUCCESS(f'Data imported successfully: {data['id']}')
+                        self.style.SUCCESS(f'Data imported successfully: {model_class.__name__} {data['id']}')
                     )
                 else:
                     self.stdout.write(
-                        self.style.WARNING(f'Data already exists: {data['id']}')
+                        self.style.WARNING(f'Data already exists: {model_class.__name__} {data['id']}')
                     )
 
     def import_coupons(self) -> None:
@@ -40,3 +43,8 @@ class Command(BaseCommand):
 
     def import_product_tax_codes(self) -> None:
         self.import_data_from_csv(f'{DATA_DIR}/product_tax_codes.csv', Tax, ['id', 'type', 'description', 'name'])
+
+    def import_items(self) -> None:
+        self.import_data_from_csv(
+            f'{DATA_DIR}/items.csv', Item, ['id', 'name', 'description', 'price', 'currency', 'tax']
+        )
